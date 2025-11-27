743c99d40fe0da8674845517a89be48a5e4b9069
Bidit das <110318738+bidit06@users.noreply.github.com>
2025-11-27 13:01:46 +0000

    Day6 complete

--- start_app.sh
+++ start_app.sh
@@ -3,7 +3,7 @@
 # Start all services in background
 livekit-server --dev &
 # To run any other agent, just change the file name or location below here:
-(cd backend && uv run python src/agent_SDR.py dev) &
+(cd backend && uv run python src/agent_fraud.py dev) &
 (cd frontend && pnpm dev) &
 
 # Wait for all background jobs
